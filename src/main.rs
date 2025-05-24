7846254053048f305584173f5bb21a3fe582f1cb
Najam <realnajmiter@gmail.com>
2025-05-25 02:50:24 +0500

    get user (dummy) data dynamically

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use axum::extract::{Json, Path, Query};
 use axum::{Router, extract::ConnectInfo, response::Html, routing::get};
+use serde::Deserialize;
+use serde_json::{Value, json};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
+#[derive(Deserialize)]
+struct Pagination {
+    page: Option<usize>,
+    per_page: Option<usize>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new().route("/", get(index));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/users/:id", get(get_user_data));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
@@ -18,20 +29,33 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn index(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Html<String> {
+#[axum::debug_handler]
+async fn get_user_data(
+    Path(user_id): Path<String>,
+    Query(pagination): Query<Pagination>,
+) -> Json<Value> {
+    let page = pagination.page.unwrap_or(1);
+    let per_page = pagination.per_page.unwrap_or(30);
+    Json(json!({
+        "user_id": user_id,
+        "page": page,
+        "per_page": per_page,
+        "items": []
+    }))
+}
+
+async fn index(ConnectInfo(_addr): ConnectInfo<SocketAddr>) -> Html<String> {
     let html = format!(
         "<!DOCTYPE html>
         <html>
             <head>
-                <title>Hello, Axum!</title>
+                <title>Simple Rust App</title>
             </head>
-            <body>
-                <h1>Hello, Axum!</h1>
-                <p>Your IP address is: {}</p>
-                <p>Welcome to the Axum web server!</p>
+            <body style=\"font-family: system-ui; font-size: 3rem;\">
+                <h1>Hello, There!</h1>
+                <p>Use <code>/users/:id?page=1&per_page=30</code> to get user data.</p>
             </body>
         </html>",
-        addr.ip()
     );
 
     Html(html)
